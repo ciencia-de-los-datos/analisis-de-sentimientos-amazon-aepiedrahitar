0bcd0b8831707555f791a008fdce0953135d866f
andres esteban piedrahita rincon <aepiedrahitar@unal.edu.co>
2022-05-24 08:26:33 -0500

    Update Lab 4 correct 3

--- preguntas.py
+++ preguntas.py
@@ -195,7 +195,7 @@ def pregunta_06():
     gridSearchCV = pregunta_04()
 
     # Cargue los datos generados en la pregunta 01.
-    x_tagged, y_tagged, X_untagged, y_untagged = pregunta_01()
+    x_tagged, y_tagged, x_untagged, y_untagged = pregunta_01()
 
     # pronostique la polaridad del sentimiento para los datos
     # no etiquetados
